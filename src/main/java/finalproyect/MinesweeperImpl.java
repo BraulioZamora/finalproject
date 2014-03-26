3c2ec8681f13b49ef211f16cbee01c6025322067
BraulioZamora <braulio_zamora@hotmail.com.ar>
2014-03-26 14:49:51 -0300

    actualizacion 2

--- src/main/java/finalproyect/MinesweeperImpl.java
+++ src/main/java/finalproyect/MinesweeperImpl.java
@@ -1,9 +1,42 @@
 package finalproyect;
+import java.util.Random;
 
+import com.despegar.highflight.utils.*;;
 public class MinesweeperImpl implements Minesweeper{
-	Cells cell;
-
-	public void uncover(int row, int col) {
+	Cells [][] grid;
+    
+    public MinesweeperImpl(int row , int col){
+    	Cells[][] grid = new Cells[row][col];
+    	int cantidadMinas = (int) Math.rint(grid.length * grid[0].length*0.15);
+    }
+	public void loadMines(int cantidadMinas)
+	  {
+			for(int i=0;i<cantidadMinas-1;i++)
+		{
+		  Matrix2DCellPosition  cellPosition= randomCell();
+		  grid [cellPosition.getRow()][cellPosition.getColumn()] = new Cells();
+		  
+		}
+	  }
+    
+	public Matrix2DCellPosition randomCell(){
+		Random rand = new Random();
+		int row = rand.nextInt(grid.length);
+		int col = rand.nextInt(grid[0].length);
+		if ( grid [row][col] == null)
+		{
+			return new Matrix2DCellPosition (row,col);
+		}
+		else {
+			 return randomCell();
+		     }
+		
+		
+    	
+    }
+	
+	
+    public void uncover(int row, int col) {
 		this.cell.uncover(row, col);
 		
 	}
@@ -46,10 +79,8 @@ public class MinesweeperImpl implements Minesweeper{
 		
 	}
 	public static void main(String[] args){
-		int row;
-		int col;
 		Minesweeper game = new MinesweeperImpl();
-		Cells[][] grid = new Cells[row][col]; 
+		
 		game.display();
 		while(!(game.isGameOver())){
 			
